24c0632c3440af2e1f8ca57116a2cd25bbd021a8
Veru7680 <veru6198@gmail.com>
2025-10-01 20:22:29 -0400

    Implementación de sidebar dinámico con menú y submenú

--- resources/views/layouts/includes/admin/sidebar.blade.php
+++ resources/views/layouts/includes/admin/sidebar.blade.php
@@ -1,18 +1,86 @@
+@php
+$links = [
+    [
+        'name'   => 'Dashboard',
+        'icon'   => 'fa-solid fa-gauge',
+        'href'   => route('admin.dashboard'),
+        'active' => request()->routeIs('admin.dashboard'),
+    ],
+
+    [
+        'header' => 'administrar paginas'
+    ],
+
+    [
+        'name'   => 'Dashboard',
+        'icon'   => 'fa-solid fa-gauge',
+        'href'   => route('admin.dashboard'),
+        'active' => false,
+        'submenu'=> [
+            [
+                'name'   => 'producto 1',
+                'href'   => '#',   
+                'active' => false,
+            ],
+              [
+                'name'   => 'producto 2',
+                'href'   => '#',   
+                'active' => false,
+            ],
+              [
+                'name'   => 'producto 3',
+                'href'   => '#',   
+                'active' => false,
+            ],
+        ],
+    ],
+
+   
+    
+];
+@endphp
+
 <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
    <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
       <ul class="space-y-2 font-medium">
+         @foreach($links as $link )
          <li>
-        <a href="#"
-            class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-            aria-label="Dashboard">
-            <i class="fa-solid fa-gauge text-xl"></i>
-            <span class="ml-3 text-base">Dashboard</span>
+             @isset($link['header'])
+               
+            <div class="px-2 py-2 text-xs font-semibold text-gray-500 uppercase ">
+               {{$link['header']}}
+            </div>
+             @else
+
+             @isset($link['submenu'])
+               <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
+                  <span class="inline-flex items-center space-x-2 text-gray-800">
+                     <i class="{{$link['icon']}}"></i>
+                  </span>
+                  <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">{{$link['name']}}</span>
+                  <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                  </svg>
+               </button>
+               <ul id="dropdown-example" class="hidden py-2 space-y-2">
+                    @foreach($link['submenu'] as $item)
+                     <li>
+                     <a href="{{$item['href']}}" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">{{$item['name']}}</a>
+                     </li> 
+                      @endforeach                   
+            </ul>
+              
+             @else
+         <a href="{{$link['href']}}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{$link['active'] ? 'bg-gray-100' : ''}}" aria-label="Dashboard">
+            <span class="inline-flex items-center space-x-2 text-gray-800">
+               <i class="{{$link['icon']}} mr-2"></i>
+               </span>
+            <span>{{$link['name']}}</span>   
          </a>
-
-
-
+         @endisset
+         @endisset
          </li>
-      
+          @endforeach
       </ul>
    </div>
 </aside>
