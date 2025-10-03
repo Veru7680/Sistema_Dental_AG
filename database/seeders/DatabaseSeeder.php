a4df1f80171a8801a546942c5a45f3754aad17ad
Veru7680 <veru6198@gmail.com>
2025-10-02 20:49:56 -0400

    Creación de seeder para roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,16 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+        $this->call(
+        RoleSeeder::class,
+        );
+
+
+
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Vero Vargas',
+            'email' => 'veru6198@gmail.com',
+            'password'=> bcrypt('10631189'),
         ]);
     }
 }
