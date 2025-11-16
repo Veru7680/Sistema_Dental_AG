f00f8228ca4e56955ee8590767045555df65c143
Veru7680 <veru6198@gmail.com>
2025-11-16 09:14:43 -0400

    (calendario): incluir FullCalendar para mostrar el calendario

--- resources/views/admin/calendar/index.blade.php
+++ resources/views/admin/calendar/index.blade.php
@@ -13,5 +13,47 @@ title=" Calendario | Dental AG"
     
     ]"  >
 
-   
+    <div x-data="data()">
+        <div x-ref='calendar'></div>
+    </div>
+
+         @push('js')
+         <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.19/index.global.min.js'></script>
+         
+         <script>
+            function data(){
+                return {
+                    init(){
+                      var calendarEl = this.$refs.calendar;
+                       var calendar = new FullCalendar.Calendar(calendarEl, {
+                       headerToolbar: {
+                            left: 'prev,next today',
+                            center: 'title',
+                            right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+                        },
+                        locale: 'es',
+                            buttonText:{
+                                today: 'Hoy',
+                                month: 'Mes',
+                                week: 'Semana',
+                                day: 'Dia',
+                                list: 'Lista'
+                            },
+                            allDayText: 'Todo el dia',
+                            noEventsText: 'No hay eventos para mostrar',
+                            
+                            initialView: 'timeGridWeek',
+                            slotDuration:'00:15:00',
+                            /* slotMinTime:'08:00:00', */
+                            slotMinTime:"{{ config('schedule.start_time')}}",
+                            /*slotMaxTime:'20:00:00',*/
+                            slotMaxTime:"{{ config('schedule.end_time')}}",
+                            srollTime:"{{ date('H:i:s')}}",
+                            });
+                            calendar.render();  
+                    }
+                }
+            }
+         </script>
+         @endpush
 </x-admin-layout>
