93106c539cf2eb86fff81dcc56e4102787afec52
Veru7680 <veru6198@gmail.com>
2025-11-17 00:20:43 -0400

    (roles-permisos): crear permisos y asignarlos a los roles

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 
@@ -14,18 +15,55 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
+        Role::create([
+        'name' => 'Admin',
+        ])->givePermissionTo(Permission::all());
         $roles=[
-            'Paciente',
-            'Recepcionista',
-            'Doctor',
-            'administrador',
+            'Paciente' =>[
+                'access_dashboard',
 
+                'create_appointment',
+                'read_appointment',
+                'read_calendar',  
+            ],
+            'Doctor'=>[
+                'access_dashboard',
+
+                'create_appointment',
+                'read_appointment',
+                'delete_appointment',
+
+                'read_calendar',  
+            ],
+            'Recepcionista'=>[
+                'access_dashboard',
+
+                'create_user',
+                'read_user',
+                'update_user',
+                'delete_user',
+
+                'read_paciente',
+                'update_paciente',
+
+                'read_doctor',
+                'update_doctor',
+
+                'create_appointment',
+                'read_appointment',
+                'update_appointment',
+                'delete_appointment',
+
+                'read_calendar', 
+            ],
+            
 
         ];
-        foreach($roles as $role){
+        foreach($roles as $role => $permissions){
             Role::create([
             'name'=>$role,
-            ]);
+            ])
+            ->givePermissionTo($permissions);
         }
     }
 }
