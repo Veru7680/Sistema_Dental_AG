4a857c10203a93bf349e2a4b0fefd3ed19aa47ab
Veru7680 <veru6198@gmail.com>
2025-12-08 15:43:47 -0400

    Diseno de Home del Sistema

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/', '/admin');
 
-/* Route::get('/', function () {
+
+Route::get('/', function () {
     return view('welcome');
-});*/
+});
 
 Route::middleware([
     'auth:sanctum',
